ef4174d95f9138daec75b9956ba23422101ea1a4
Honghyeon Yoo <neo7k@u.sogang.ac.kr>
2023-05-07 01:59:30 +0900

    branch PR test4

--- app/src/main/java/com/sgcd/insubunhae/MainActivity.java
+++ app/src/main/java/com/sgcd/insubunhae/MainActivity.java
@@ -26,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
     private Cursor dbCursor;
 
     //PR test comment2
-    // merge this to feature
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
