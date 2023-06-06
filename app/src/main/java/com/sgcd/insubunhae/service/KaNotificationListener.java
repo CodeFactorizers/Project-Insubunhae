6187543f927368793117f505c37867e6802d181f
Honghyeon Yoo <neo7k@u.sogang.ac.kr>
2023-06-06 15:59:15 +0900

    sense Kakao Talk message through notification service

---
+++ app/src/main/java/com/sgcd/insubunhae/service/KaNotificationListener.java
@@ -0,0 +1,41 @@
+package com.sgcd.insubunhae.service;
+
+import android.app.Notification;
+import android.os.Bundle;
+import android.service.notification.NotificationListenerService;
+import android.service.notification.StatusBarNotification;
+import android.text.TextUtils;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.util.StringTokenizer;
+
+public class KaNotificationListener extends NotificationListenerService {
+    private static final String TAG = KaNotificationListener.class.getSimpleName();
+
+    @Override
+    public void onNotificationPosted(StatusBarNotification sbn) {
+        final String packageName = sbn.getPackageName();
+        Notification notification=sbn.getNotification();
+        Bundle extras = notification.extras;
+        String id=null;
+        String text=null;
+
+        if(!TextUtils.isEmpty(packageName) && packageName.equals("com.kakao.talk")) {
+            Toast.makeText(this, "Kakao Talk message read", Toast.LENGTH_SHORT).show();
+            Log.d(TAG, "onNotificationPosted");
+
+            if(sbn.getPackageName().equalsIgnoreCase("com.kakao.talk")) {
+                id=extras.getString(Notification.EXTRA_TITLE);
+                text=extras.getString(Notification.EXTRA_TEXT);
+                //Toast.makeText(this, "kakao>> " + "Title: " + id + ", Text: " + text, Toast.LENGTH_SHORT).show();
+                Log.d(TAG, "kakao>> " + "Title: " + id + ", Text: " + text);
+            }
+        }
+    }
+
+    @Override
+    public void onNotificationRemoved(StatusBarNotification sbn) {
+        Toast.makeText(this, "KaNotificationListner 2", Toast.LENGTH_SHORT).show();
+    }
+}
