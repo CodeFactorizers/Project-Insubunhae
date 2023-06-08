288bd28de0b3ff8bc46cfc881313f7c938977d20
extinctmule <extinctmule@gmail.com>
2023-06-08 11:38:10 +0900

    Temporal commit

--- app/src/main/java/com/sgcd/insubunhae/base/Animal.java
+++ app/src/main/java/com/sgcd/insubunhae/base/Animal.java
@@ -1,5 +1,7 @@
 package com.sgcd.insubunhae.base;
 
+import com.sgcd.insubunhae.R;
+
 /**
  * @Author: 怪兽N
  * @Time: 2021/5/7  19:12
@@ -9,8 +11,17 @@ package com.sgcd.insubunhae.base;
  * node bean
  */
 public class Animal {
-    public int headId;
+    public int headId = R.drawable.baseline_person_outline_48;
     public String name;
+    public String groupName;
+    public String phoneNumber;
+
+
+    // 생성자1
+    public Animal(String name) {
+        this.name = name;
+    }
+    //생성자2
     public Animal(int headId, String name) {
         this.headId = headId;
 
@@ -26,8 +37,76 @@ public class Animal {
         this.name = modifiedName.toString().trim();
     }
 
+    public void setIcon(int id){
+        this.headId = id;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getName(){
+        return name;
+    }
+    public void setGroupName(String groupName){
+        this.groupName = groupName;
+    }
+    public String getGroupName(){
+        return groupName;
+    }
+    public void setPhoneNumber(String phoneNumber){
+        this.phoneNumber = phoneNumber;
+    }
+
     @Override
     public String toString() {
         return "Animal["+name+"]";
     }
 }
+
+
+/*
+public class Animal extends Fragment {
+
+
+    public int icon = R.drawable.btn_radio_off_mtrl;
+    public String name;
+    public String groupName;
+    public String phoneNumber;
+
+    public Animal(String name) {
+        this.name = name;
+    }
+    public Animal(int icon, String name){
+        this.icon = icon;
+        this.name = name;
+    }
+
+    public void setIcon(int id){
+        this.icon = id;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getName(){
+        return name;
+    }
+    public void setGroupName(String groupName){
+        this.groupName = groupName;
+    }
+    public String getGroupName(){
+        return groupName;
+    }
+    public void setPhoneNumber(String phoneNumber){
+        this.phoneNumber = phoneNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "Animal[" + name + "]";
+    }
+}
+
+
+
+
+
+ */
--- app/src/main/java/com/sgcd/insubunhae/ui/home/HomeFragment.java
+++ app/src/main/java/com/sgcd/insubunhae/ui/home/HomeFragment.java
@@ -140,7 +140,7 @@ public class HomeFragment extends Fragment {
         // add some nodes
         binding.addNodesBt.setOnClickListener(v -> {
             if (targetNode == null) {
-                Toast.makeText(requireContext(), "Ohs, your targetNode is null", Toast.LENGTH_SHORT).show();
+                Toast.makeText(requireContext(), /*"Ohs, your targetNode is null"*/"타겟 노드 없음", Toast.LENGTH_SHORT).show();
                 return;
             }
             NodeModel<Animal> a = new NodeModel<>(new Animal(R.drawable.ic_10, "add-" + atomicInteger.getAndIncrement()));
@@ -156,7 +156,7 @@ public class HomeFragment extends Fragment {
         // remove node
         binding.removeNodeBt.setOnClickListener(v -> {
             if (removeCache.isEmpty()) {
-                Toast.makeText(requireContext(), "Ohs, demo removeCache is empty now!! Try to add some nodes firstly!!", Toast.LENGTH_SHORT).show();
+                Toast.makeText(requireContext(), /*"Ohs, demo removeCache is empty now!! Try to add some nodes firstly!!"*/"제거 캐시 없음", Toast.LENGTH_SHORT).show();
                 return;
             }
             NodeModel<Animal> toRemoveNode = removeCache.pop();
@@ -166,7 +166,7 @@ public class HomeFragment extends Fragment {
 
         adapter.setOnItemListener((item, node) -> {
             Animal animal = node.getValue();
-            Toast.makeText(requireContext(), "you click the head of " + animal, Toast.LENGTH_SHORT).show();
+            Toast.makeText(requireContext(), "선택: " + animal, Toast.LENGTH_SHORT).show();
         });
 
         // treeView control listener
@@ -210,7 +210,7 @@ public class HomeFragment extends Fragment {
         //return new BoxDownTreeLayoutManager(requireContext(),space_50dp,space_20dp,line);
         //return new BoxLeftTreeLayoutManager(this,space_50dp,space_20dp,line);
         //return new BoxUpTreeLayoutManager(this,space_50dp,space_20dp,line);
-        //return new BoxHorizonLeftAndRightLayoutManager(requireContext(),space_count,space_20dp,line);
+        return new BoxHorizonLeftAndRightLayoutManager(requireContext(),space_count,space_20dp,line);
         //return new BoxVerticalUpAndDownLayoutManager(requireContext(),space_30dp,space_20dp,line);
 
 
@@ -230,7 +230,7 @@ public class HomeFragment extends Fragment {
         //return new CompactVerticalUpAndDownLayoutManager(requireContext(),space_count,space_20dp,line);
 
         //return new CompactRingTreeLayoutManager(requireContext(), space_30dp,space_20dp,line);
-        return new ForceDirectedTreeLayoutManager(requireContext(),line);
+        //return new ForceDirectedTreeLayoutManager(requireContext(),line);
     }
 
     private BaseLine getLine() {
@@ -241,85 +241,213 @@ public class HomeFragment extends Fragment {
     }
 
     private void setData(AnimalTreeViewAdapter adapter){
-        //root
+        NodeModel<Animal> me = new NodeModel<>(new Animal(R.drawable.baseline_person_pin_circle_48,"나"));
+        NodeModel<Animal> family = new NodeModel<>(new Animal(R.drawable.baseline_groups_48,"가족"));
 
-        NodeModel<Animal> root = new NodeModel<>(new Animal(R.drawable.ic_01,"내가루트여야지"));
-        TreeModel<Animal> treeModel = new TreeModel<>(root);
+        NodeModel<Animal> sogang_univ =new NodeModel<>(new Animal(R.drawable.baseline_school_48,"서강대"));
+        NodeModel<Animal> insu = new NodeModel<>(new Animal(R.drawable.baseline_install_mobile_48,"캡디1 인수분해 팀"));
+        NodeModel<Animal> dongari = new NodeModel<>(new Animal(R.drawable.baseline_groups_48,"동아리"));
+        NodeModel<Animal> colleague = new NodeModel<>(new Animal(R.drawable.baseline_groups_48,"동기"));
 
+        NodeModel<Animal> school_reunion = new NodeModel<>(new Animal(R.drawable.baseline_groups_48,"동창"));
+        NodeModel<Animal> high_school = new NodeModel<>(new Animal(R.drawable.baseline_groups_48,"고등학교"));
+        NodeModel<Animal> middle_school = new NodeModel<>(new Animal(R.drawable.baseline_groups_48,"중학교"));
+
+        NodeModel<Animal> gatherings = new NodeModel<>(new Animal(R.drawable.baseline_groups_48,"모임"));
+        NodeModel<Animal> study_group = new NodeModel<>(new Animal(R.drawable.baseline_groups_48,"캡디1 스터디"));
+        NodeModel<Animal> photo_club = new NodeModel<>(new Animal(R.drawable.baseline_photo_camera_48,"사진 동호회"));
+
+        NodeModel<Animal> parttime_job = new NodeModel<>(new Animal(R.drawable.baseline_groups_48,"알바"));
+
+        NodeModel<Animal> not_assigned = new NodeModel<>(new Animal(R.drawable.baseline_group_off_48,"그룹 미분류"));
+
+        TreeModel<Animal> Me = new TreeModel<>(me);
+
+        TreeModel<Animal> Family = new TreeModel<>(family);
+
+        TreeModel<Animal> SogangUniv = new TreeModel<>(sogang_univ);
+        TreeModel<Animal> Insu = new TreeModel<>(insu);
+        //TreeModel<Animal> Sogong = new TreeModel<>(sogong);
+        TreeModel<Animal> Dongari = new TreeModel<>(dongari);
+        TreeModel<Animal> Colleague = new TreeModel<>(colleague);
+
+        TreeModel<Animal> SchoolReunion= new TreeModel<>(school_reunion);
+        TreeModel<Animal> MiddleSchool= new TreeModel<>(high_school);
+        TreeModel<Animal> HighSchool= new TreeModel<>(middle_school);
+
+        TreeModel<Animal> Gatherings = new TreeModel<>(gatherings);
+        TreeModel<Animal> StudyGroup = new TreeModel<>(study_group);
+        TreeModel<Animal> PhotoClub = new TreeModel<>(photo_club);
+        TreeModel<Animal> ParttimeJob = new TreeModel<>(parttime_job);
+        TreeModel<Animal> NotAssigned= new TreeModel<>(not_assigned);
 
 
         //TODO: 그룹명 get해서 TreeModel<Group> 하기, 모든 연락처에 대해 nodeModel하기, for(count){NodeModel<Contacts>, treemodel.addnode } 하기
-        NodeModel<Animal> Insu = new NodeModel<>(new Animal(R.drawable.ic_05,"캡디1\n인수분해 팀"));
-        NodeModel<Animal> Sogong_10_people = new NodeModel<>(new Animal(R.drawable.ic_02,"소공"));
-        NodeModel<Animal> Dongari = new NodeModel<>(new Animal(R.drawable.ic_06,"동아리"));
-
 
         //child nodes
+        NodeModel<Animal> family_1 = new NodeModel<>(new Animal("동생"));
+        NodeModel<Animal> family_2 = new NodeModel<>(new Animal("엄마"));
+        NodeModel<Animal> family_3 = new NodeModel<>(new Animal("아빠"));
+        NodeModel<Animal> insu_1 = new NodeModel<>(new Animal("17유홍현"));
+        NodeModel<Animal> insu_2 = new NodeModel<>(new Animal("17조세희"));
+        NodeModel<Animal> insu_3 = new NodeModel<>(new Animal("18김준수"));
+        NodeModel<Animal> insu_4 = new NodeModel<>(new Animal("19임수민"));
+        NodeModel<Animal> sogong_1 = new NodeModel<>(new Animal("소공팀원1"));
+        NodeModel<Animal> sogong_2 = new NodeModel<>(new Animal("소공팀원2"));
+        NodeModel<Animal> sogong_3 = new NodeModel<>(new Animal("소공팀원3"));
+        NodeModel<Animal> sogong_4 = new NodeModel<>(new Animal("소공팀원4"));
+        NodeModel<Animal> sogong_5 = new NodeModel<>(new Animal("소공팀원5"));
+        NodeModel<Animal> sogong_6 = new NodeModel<>(new Animal("소공팀원6"));
+        NodeModel<Animal> sogong_7 = new NodeModel<>(new Animal("소공팀원7"));
+        NodeModel<Animal> sogong_8 = new NodeModel<>(new Animal("소공팀원8"));
+        NodeModel<Animal> sogong_9 = new NodeModel<>(new Animal("소공팀원9"));
+        NodeModel<Animal> sogong_10 = new NodeModel<>(new Animal("소공팀원10"));
+        NodeModel<Animal> dong_1 = new NodeModel<>(new Animal("동아리원1"));
+        NodeModel<Animal> dong_2 = new NodeModel<>(new Animal("동아리원2"));
+        NodeModel<Animal> dong_3 = new NodeModel<>(new Animal("동아리원3"));
+        NodeModel<Animal> dong_4 = new NodeModel<>(new Animal("동아리원4"));
+        NodeModel<Animal> colleague_1 = new NodeModel<>(new Animal("동기1"));
+        NodeModel<Animal> colleague_2 = new NodeModel<>(new Animal("동기2"));
+        NodeModel<Animal> colleague_3 = new NodeModel<>(new Animal("동기3"));
+        NodeModel<Animal> colleague_4 = new NodeModel<>(new Animal("동기4"));
+        NodeModel<Animal> colleague_5 = new NodeModel<>(new Animal("동기5"));
 
-        NodeModel<Animal> insu_1 = new NodeModel<>(new Animal(R.drawable.btn_radio_off_mtrl,"17유홍현"));
-        NodeModel<Animal> insu_2 = new NodeModel<>(new Animal(R.drawable.btn_radio_off_mtrl,"17조세희"));
-        NodeModel<Animal> insu_3 = new NodeModel<>(new Animal(R.drawable.btn_radio_off_mtrl,"18김준수"));
-        NodeModel<Animal> insu_4 = new NodeModel<>(new Animal(R.drawable.btn_radio_off_mtrl,"19임수민"));
 
-
-        NodeModel<Animal> sogong_1 = new NodeModel<>(new Animal(R.drawable.btn_radio_off_mtrl,"소공팀원1"));
-        NodeModel<Animal> sogong_2 = new NodeModel<>(new Animal(R.drawable.btn_radio_off_mtrl,"소공팀원2"));
-        NodeModel<Animal> sogong_3 = new NodeModel<>(new Animal(R.drawable.btn_radio_off_mtrl,"소공팀원3"));
-        NodeModel<Animal> sogong_4 = new NodeModel<>(new Animal(R.drawable.btn_radio_off_mtrl,"소공팀원4"));
-        NodeModel<Animal> sogong_5 = new NodeModel<>(new Animal(R.drawable.btn_radio_off_mtrl,"소공팀원5"));
-        NodeModel<Animal> sogong_6 = new NodeModel<>(new Animal(R.drawable.btn_radio_off_mtrl,"소공팀원6"));
-        NodeModel<Animal> sogong_7 = new NodeModel<>(new Animal(R.drawable.btn_radio_off_mtrl,"소공팀원7"));
-        NodeModel<Animal> sogong_8 = new NodeModel<>(new Animal(R.drawable.btn_radio_off_mtrl,"소공팀원8"));
-        NodeModel<Animal> sogong_9 = new NodeModel<>(new Animal(R.drawable.btn_radio_off_mtrl,"소공팀원9"));
-        NodeModel<Animal> sogong_10 = new NodeModel<>(new Animal(R.drawable.btn_radio_off_mtrl,"소공팀원10"));
-
-
-        NodeModel<Animal> dong_1 = new NodeModel<>(new Animal(R.drawable.btn_radio_off_mtrl,"동아리원1"));
-        NodeModel<Animal> dong_2 = new NodeModel<>(new Animal(R.drawable.btn_radio_off_mtrl,"동아리원2"));
-        NodeModel<Animal> dong_3 = new NodeModel<>(new Animal(R.drawable.btn_radio_off_mtrl,"동아리원3"));
-        NodeModel<Animal> dong_4 = new NodeModel<>(new Animal(R.drawable.btn_radio_off_mtrl,"동아리원4"));
-        NodeModel<Animal> dong_5 = new NodeModel<>(new Animal(R.drawable.btn_radio_off_mtrl,"동아리원5"));
-        NodeModel<Animal> dong_6 = new NodeModel<>(new Animal(R.drawable.btn_radio_off_mtrl,"동아리원6"));
-        NodeModel<Animal> dong_7 = new NodeModel<>(new Animal(R.drawable.btn_radio_off_mtrl,"동아리원7"));
-        NodeModel<Animal> dong_8 = new NodeModel<>(new Animal(R.drawable.btn_radio_off_mtrl,"동아리원8"));
-        NodeModel<Animal> dong_9 = new NodeModel<>(new Animal(R.drawable.btn_radio_off_mtrl,"동아리원9"));
-        NodeModel<Animal> dong_10 = new NodeModel<>(new Animal(R.drawable.btn_radio_off_mtrl,"동아리원10"));
-        NodeModel<Animal> dong_11 = new NodeModel<>(new Animal(R.drawable.btn_radio_off_mtrl,"동아리원11"));
+        NodeModel<Animal> h_school_1 = new NodeModel<>(new Animal("고등동창1"));
+        NodeModel<Animal> h_school_2 = new NodeModel<>(new Animal("고등동창2"));
+        NodeModel<Animal> h_school_3 = new NodeModel<>(new Animal("고등동창3"));
+        NodeModel<Animal> m_school_1 = new NodeModel<>(new Animal("중등동창1"));
+        NodeModel<Animal> m_school_2 = new NodeModel<>(new Animal("중등동창2"));
+        NodeModel<Animal> m_school_3 = new NodeModel<>(new Animal("중등동창3"));
+        NodeModel<Animal> study_group_1 = new NodeModel<>(new Animal("스터디원1"));
+        NodeModel<Animal> study_group_2 = new NodeModel<>(new Animal("스터디원2"));
+        NodeModel<Animal> study_group_3 = new NodeModel<>(new Animal("스터디원3"));
+        NodeModel<Animal> photo_club_1 = new NodeModel<>(new Animal("동호회 회원1"));
+        NodeModel<Animal> photo_club_2 = new NodeModel<>(new Animal("동호회 회원2"));
+        NodeModel<Animal> photo_club_3 = new NodeModel<>(new Animal("동호회 회원3"));
+        NodeModel<Animal> parttime_job_1 = new NodeModel<>(new Animal("알바 동료1"));
+        NodeModel<Animal> parttime_job_2 = new NodeModel<>(new Animal("알바 동료2"));
+        NodeModel<Animal> parttime_job_3 = new NodeModel<>(new Animal("알바 동료3"));
+        NodeModel<Animal> not_assigned_1 = new NodeModel<>(new Animal("기타1"));
+        NodeModel<Animal> not_assigned_2 = new NodeModel<>(new Animal("기타2"));
+        NodeModel<Animal> not_assigned_3 = new NodeModel<>(new Animal("기타3"));
 
         //build relationship
-        treeModel.addNode(root,Insu,Sogong_10_people,Dongari);
-        treeModel.addNode(Insu,insu_1,insu_2,insu_3,insu_4);
-        //treeModel.addNode(Insu,insu_3);
-        //treeModel.addNode(Insu,insu_4);
-        treeModel.addNode(Dongari,dong_1,dong_2,dong_3,dong_4,dong_5,dong_6,dong_7,dong_8,dong_9,dong_10,dong_11);
-        treeModel.addNode(Sogong_10_people,sogong_1,sogong_2,sogong_3,sogong_4, sogong_5, sogong_6, sogong_7, sogong_8, sogong_9, sogong_10);
+        Me.addNode(me,family, sogang_univ, school_reunion, gatherings, parttime_job, not_assigned);
 
+        Family.addNode(family, family_1,family_2, family_3);
 
-        //sehee's try
-        Log.d("0608", "home frag");
-        ArrayList<Contact> contactsList = ((MainActivity)getActivity()).getContactsList().getContactsList();
+        SogangUniv.addNode(sogang_univ, insu, dongari, colleague);
+        Insu.addNode(insu, insu_1, insu_2, insu_3, insu_4);
+        Dongari.addNode(dongari,dong_1,dong_2,dong_3);
+        Colleague.addNode(colleague, colleague_1, colleague_2, colleague_3, colleague_4);
 
-        String[] contactNameArray = new String[contactsList.size()];
-        ArrayList<NodeModel<Animal>> nodeList = new ArrayList<>();
-        String[] contactIdArray = new String[contactsList.size()];
-        int[] contactIdIntArray = new int[contactIdArray.length];
-        for (int i = 0; i < contactsList.size(); i++) {
-            contactNameArray[i] = contactsList.get(i).getName();
-            contactIdArray[i] = contactsList.get(i).getId();
-            contactIdIntArray[i] = Integer.parseInt(contactIdArray[i]);
+        SchoolReunion.addNode(school_reunion, high_school, middle_school);
+        HighSchool.addNode(high_school, h_school_1, h_school_2, h_school_3);
+        MiddleSchool.addNode(middle_school, m_school_1, m_school_2);
 
-            nodeList.add(new NodeModel<>(new Animal(R.drawable.icon_user_5, contactNameArray[i])));
-            //treeModel.addNode(root, sub0, sub1, nodeList.get(i));
-        }
+        Gatherings.addNode(gatherings, study_group, photo_club);
+        StudyGroup.addNode(study_group, study_group_1, study_group_2, study_group_3);
 
+        PhotoClub.addNode(photo_club, photo_club_1,photo_club_2,photo_club_3);
+
+        ParttimeJob.addNode(parttime_job, parttime_job_1,parttime_job_2);
+        NotAssigned.addNode(not_assigned, not_assigned_1,not_assigned_2,not_assigned_3);
 
         //mark
-        parentToRemoveChildren = Sogong_10_people;
-        targetNode = sogong_1;
+        parentToRemoveChildren = parttime_job;
+        targetNode = parttime_job_3;
 
 
         //set data
-        adapter.setTreeModel(treeModel);
+        adapter.setTreeModel(Me);
+
+
+
+
+
+
+
+
+//        //root
+//
+//        NodeModel<Animal> root = new NodeModel<>(new Animal(R.drawable.ic_01,"내가루트여야지"));
+//        TreeModel<Animal> treeModel = new TreeModel<>(root);
+//
+//
+//
+//        //TODO: 그룹명 get해서 TreeModel<Group> 하기, 모든 연락처에 대해 nodeModel하기, for(count){NodeModel<Contacts>, treemodel.addnode } 하기
+//        NodeModel<Animal> Insu = new NodeModel<>(new Animal(R.drawable.ic_05,"캡디1\n인수분해 팀"));
+//        NodeModel<Animal> Sogong_10_people = new NodeModel<>(new Animal(R.drawable.ic_02,"소공"));
+//        NodeModel<Animal> Dongari = new NodeModel<>(new Animal(R.drawable.ic_06,"동아리"));
+//
+//
+//        //child nodes
+//
+//        NodeModel<Animal> insu_1 = new NodeModel<>(new Animal(R.drawable.btn_radio_off_mtrl,"17유홍현"));
+//        NodeModel<Animal> insu_2 = new NodeModel<>(new Animal(R.drawable.btn_radio_off_mtrl,"17조세희"));
+//        NodeModel<Animal> insu_3 = new NodeModel<>(new Animal(R.drawable.btn_radio_off_mtrl,"18김준수"));
+//        NodeModel<Animal> insu_4 = new NodeModel<>(new Animal(R.drawable.btn_radio_off_mtrl,"19임수민"));
+//
+//
+//        NodeModel<Animal> sogong_1 = new NodeModel<>(new Animal(R.drawable.btn_radio_off_mtrl,"소공팀원1"));
+//        NodeModel<Animal> sogong_2 = new NodeModel<>(new Animal(R.drawable.btn_radio_off_mtrl,"소공팀원2"));
+//        NodeModel<Animal> sogong_3 = new NodeModel<>(new Animal(R.drawable.btn_radio_off_mtrl,"소공팀원3"));
+//        NodeModel<Animal> sogong_4 = new NodeModel<>(new Animal(R.drawable.btn_radio_off_mtrl,"소공팀원4"));
+//        NodeModel<Animal> sogong_5 = new NodeModel<>(new Animal(R.drawable.btn_radio_off_mtrl,"소공팀원5"));
+//        NodeModel<Animal> sogong_6 = new NodeModel<>(new Animal(R.drawable.btn_radio_off_mtrl,"소공팀원6"));
+//        NodeModel<Animal> sogong_7 = new NodeModel<>(new Animal(R.drawable.btn_radio_off_mtrl,"소공팀원7"));
+//        NodeModel<Animal> sogong_8 = new NodeModel<>(new Animal(R.drawable.btn_radio_off_mtrl,"소공팀원8"));
+//        NodeModel<Animal> sogong_9 = new NodeModel<>(new Animal(R.drawable.btn_radio_off_mtrl,"소공팀원9"));
+//        NodeModel<Animal> sogong_10 = new NodeModel<>(new Animal(R.drawable.btn_radio_off_mtrl,"소공팀원10"));
+//
+//
+//        NodeModel<Animal> dong_1 = new NodeModel<>(new Animal(R.drawable.btn_radio_off_mtrl,"동아리원1"));
+//        NodeModel<Animal> dong_2 = new NodeModel<>(new Animal(R.drawable.btn_radio_off_mtrl,"동아리원2"));
+//        NodeModel<Animal> dong_3 = new NodeModel<>(new Animal(R.drawable.btn_radio_off_mtrl,"동아리원3"));
+//        NodeModel<Animal> dong_4 = new NodeModel<>(new Animal(R.drawable.btn_radio_off_mtrl,"동아리원4"));
+//        NodeModel<Animal> dong_5 = new NodeModel<>(new Animal(R.drawable.btn_radio_off_mtrl,"동아리원5"));
+//        NodeModel<Animal> dong_6 = new NodeModel<>(new Animal(R.drawable.btn_radio_off_mtrl,"동아리원6"));
+//        NodeModel<Animal> dong_7 = new NodeModel<>(new Animal(R.drawable.btn_radio_off_mtrl,"동아리원7"));
+//        NodeModel<Animal> dong_8 = new NodeModel<>(new Animal(R.drawable.btn_radio_off_mtrl,"동아리원8"));
+//        NodeModel<Animal> dong_9 = new NodeModel<>(new Animal(R.drawable.btn_radio_off_mtrl,"동아리원9"));
+//        NodeModel<Animal> dong_10 = new NodeModel<>(new Animal(R.drawable.btn_radio_off_mtrl,"동아리원10"));
+//        NodeModel<Animal> dong_11 = new NodeModel<>(new Animal(R.drawable.btn_radio_off_mtrl,"동아리원11"));
+//
+//        //build relationship
+//        treeModel.addNode(root,Insu,Sogong_10_people,Dongari);
+//        treeModel.addNode(Insu,insu_1,insu_2,insu_3,insu_4);
+//        //treeModel.addNode(Insu,insu_3);
+//        //treeModel.addNode(Insu,insu_4);
+//        treeModel.addNode(Dongari,dong_1,dong_2,dong_3,dong_4,dong_5,dong_6,dong_7,dong_8,dong_9,dong_10,dong_11);
+//        treeModel.addNode(Sogong_10_people,sogong_1,sogong_2,sogong_3,sogong_4, sogong_5, sogong_6, sogong_7, sogong_8, sogong_9, sogong_10);
+//
+//
+//        //sehee's try
+//        Log.d("0608", "home frag");
+//        ArrayList<Contact> contactsList = ((MainActivity)getActivity()).getContactsList().getContactsList();
+//
+//        String[] contactNameArray = new String[contactsList.size()];
+//        ArrayList<NodeModel<Animal>> nodeList = new ArrayList<>();
+//        String[] contactIdArray = new String[contactsList.size()];
+//        int[] contactIdIntArray = new int[contactIdArray.length];
+//        for (int i = 0; i < contactsList.size(); i++) {
+//            contactNameArray[i] = contactsList.get(i).getName();
+//            contactIdArray[i] = contactsList.get(i).getId();
+//            contactIdIntArray[i] = Integer.parseInt(contactIdArray[i]);
+//
+//            nodeList.add(new NodeModel<>(new Animal(R.drawable.icon_user_5, contactNameArray[i])));
+//            //treeModel.addNode(root, sub0, sub1, nodeList.get(i));
+//        }
+//
+//
+//        //mark
+//        parentToRemoveChildren = Sogong_10_people;
+//        targetNode = sogong_1;
+//
+//
+//        //set data
+//        adapter.setTreeModel(treeModel);
     }
 
 
