97db6866ae856f70008f5df702158a4c8277ad1b
extinctmule <extinctmule@gmail.com>
2023-06-08 11:45:33 +0900

    Add: drawables

---
+++ app/src/main/java/com/sgcd/insubunhae/base/AnimalGenerator.java
@@ -0,0 +1,74 @@
+package com.sgcd.insubunhae.base;
+
+import android.os.Parcelable;
+
+import com.sgcd.insubunhae.db.Contact;
+import com.sgcd.insubunhae.db.ContactsList;
+import com.sgcd.insubunhae.db.Group;
+import com.sgcd.insubunhae.base.Animal;
+
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class AnimalGenerator extends ContactsList {
+    public ArrayList<Contact> animals_list = new ArrayList<Contact>();
+
+    @Override
+    public ArrayList<Contact> getContactsList() {
+        return animals_list;
+    }
+
+    public int animals_count = animals_list.size();
+
+
+//
+//    for(int i = 0; i<animals_count; i++){
+//
+//    }
+//
+
+}
+
+
+//
+//// Zoo: Group of Animal
+//    public class Zoo extends Group{
+//        //SQLiteDatabase db;
+////    void getContactFromAppDB(SQLiteDatabase db){
+////
+////    }
+////    public ArrayList<Contact>
+////    getContactsList();
+//        private String ZooId;
+//        public String ZooName;
+//        public int AnimalCount;
+//        public ArrayList<String> AnimalList;// = new ArrayList<>();
+//
+//        Zoo() {
+//            ZooId = super.getGroupId();
+//            ZooName = super.getGroupName();
+//            AnimalList = super.getMemberList();
+//            AnimalCount =
+//        }
+//
+//        @Override
+//        public String toString() {
+//            return "Zoo["+ZooName+"]";
+//        }
+//
+//
+//    }
+
+//        StringBuilder modifiedName = new StringBuilder();
+//        String[] words = name.split(" ");
+//        for (String word : words) {
+//            if (word.length() > 7) {
+//                modifiedName.append(word.replace(' ', '\n')).append(" ");
+//            } else {
+//                modifiedName.append(word).append(" ");
+//            }
+//        }
+//        this.name = modifiedName.toString().trim();
+//
+//
